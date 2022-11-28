b345b4666ef6697a01c257fb022965d3487e5ecc
Rodrigo Galdino <galdinorodrigo553@gmail.com>
2022-11-27 21:29:36 -0300

    try to send request

--- app/src/main/java/com/again/qrcodgen/MainActivity.java
+++ app/src/main/java/com/again/qrcodgen/MainActivity.java
@@ -7,6 +7,18 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.SocketTimeoutException;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
 public class MainActivity extends AppCompatActivity {
     EditText edit_link;
     @Override
@@ -21,5 +33,47 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, "Hello world!", Toast.LENGTH_SHORT).show();
     }
 
+    public void request() throws IOException {
+        URL url = new URL("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"); //http://qrcod-gen.herokuapp.com/qrcode/show?
+        HttpURLConnection client = null;
+        try {
+            client = (HttpURLConnection) url.openConnection();
+            client.setRequestMethod("GET");
+            client.setRequestProperty("content", edit_link.getText().toString());
+            client.setDoOutput(true);
+
+            OutputStream outputPost = new BufferedOutputStream(client.getOutputStream());
+            writeStream(outputPost);
+            outputPost.flush();
+            outputPost.close();
+        } catch(MalformedURLException error) {
+            //Handles an incorrectly entered URL
+        } catch(SocketTimeoutException error) {
+            //Handles URL access timeout.
+        } catch (IOException error) {
+            //Handles input and output errors
+        } finally {
+            if(client != null) // Make sure the connection is not null.
+            {
+                client.disconnect();
+            }
+        }
+    }
+
+    private void writeStream(OutputStream outputPost) {
+    }
+
+    /*public void request() throws IOException {
+        URL url = new URL("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"); //"http://qrcod-gen.herokuapp.com/qrcode/show?content="+edit_link.getText().toString());
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+            readStream(in);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            urlConnection.disconnect();
+        }
+    }*/
 
 }
